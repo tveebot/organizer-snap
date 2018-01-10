8e62099fe3bbce2c001182fab45674fb320a2ee7
David Fialho <fialho.david@protonmail.com>
2018-01-10 23:52:35 +0000

    Change confinement to classic

--- files/bin/wrapper.sh
+++ files/bin/wrapper.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
+export PATH="$SNAP/usr/sbin:$SNAP/usr/bin:$SNAP/sbin:$SNAP/bin:$PATH"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SNAP/lib:$SNAP/usr/lib:$SNAP/lib/x86_64-linux-gnu:$SNAP/usr/lib/x86_64-linux-gnu"
+export LD_LIBRARY_PATH="$SNAP/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH=$SNAP_LIBRARY_PATH:$LD_LIBRARY_PATH
+
 $SNAP/bin/tveebot-organizer --conf $SNAP_DATA/config.ini --log $SNAP_DATA/organizer.log
